e86491a1da42141867f7650415fbfa728e6943b8
Aryan Knight <aryan313122@gmail.com>
2021-12-07 23:58:14 +0530

    first commit

---
+++ src/Components/Intro/Intro.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Intro.css";
+
+export default function Intro() {
+    return (
+        <div className="container-medium">
+            <div className="intro">
+            <div className="intro-left">
+                <div style={{padding:'1rem',width:'100%'}}>
+                    <div className="intro-head">
+                        Grow you business startup system better
+                    </div>
+                    <div className="intro-text">
+                        Our passion to work hard and deliver excellent results. It could solve the needs of your customers and develop innovation.
+                    </div>
+                    <button className="intro-button">
+                        GET STARTED
+                    </button>
+                </div>
+            </div>
+            <div className="intro-right">
+
+            </div>
+            </div>
+        </div>
+        
+    )
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Logo from '../../Images/logo.png';
+import Twitter from '../../Images/twitter.png'
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+            <div className="container-medium">
+                <div className="navbar">
+                    <div className="navbar-logo">
+                        <img src={Logo} id="nav-logo"/>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="nav-item">Overview</div>
+                        <div className="nav-item">Features</div>
+                        <div className="nav-item">Roadmap</div>
+                    </div>
+                    <div className="navbar-menu">
+                        <div className="nav-icons">
+                            <img src={Twitter} className="nav-icon"/> 
+                        </div>  
+                        <div className="nav-icons">
+                            <img src={Twitter} className="nav-icon"/> 
+                        </div>  
+                        <div className="nav-icons">
+                            <img src={Twitter} className="nav-icon"/> 
+                        </div>                
+                    </div>
+                </div>
+            </div>
+    )
+}
